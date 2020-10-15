ce4a5b32334f9b8b2614ff0e138fd128ec2e9520
Guerder Vieira <guerder.vieira@geen.com.br>
2020-10-15 08:43:43 -0300

    Corrigido tamanho do container quando não possuir divisores. Refatorado elementos this.

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ $(document).ready(function () {
 
 const createSections = function () {
   $('.sections').each(function () {
-    $(this).scrollTop(0);
-    const defaultHeightSection = $(this).attr('data-height-section');
-    const itemsSection = $(this).attr('data-items-section');
+    const section = $(this);
+    section.scrollTop(0);
+    const defaultHeightSection = section.attr('data-height-section');
+    const itemsSection = section.attr('data-items-section');
 
-    const container = $(this).find('div').first();
-    const elements = $(container).children('div');
+    const container = section.find('div').first();
+    const elements = container.children('div');
 
     const dividersRemove = container.find('.section-divider');
     dividersRemove.each(function () {
@@ -20,59 +21,72 @@ const createSections = function () {
     });
 
     $.each(elements, function (index) {
+      const element = $(this);
       let position = index + 1;
       if (position % itemsSection === 0 && position !== elements.length) {
-        $(this).after(
+        $(element).after(
           '<hr class="section-divider" style="display: flex; width: 100%; margin: 20px 0; border: 0;" />'
         );
       }
     });
 
-    const dividers = $(this).find('.section-divider');
+    const dividers = section.find('.section-divider');
     if (dividers.length == 0) {
-      $(container).css('height', defaultHeightSection + 'px');
-      $(this).attr('data-total-sections', 0);
-      $(this).attr('data-total-sections', 0);
-      $(this).attr('data-positions', 0);
+      container.css('height', defaultHeightSection + 'px');
+      section.attr('data-total-sections', 0);
+      section.attr('data-total-sections', 0);
+      section.attr('data-positions', 0);
+
+      const firstBox = container.find('div').first();
+      const topFirstBox = firstBox.offset().top;
+      const padding = topFirstBox - $(this).offset().top;
+      const lastBox = container.find('div').last();
+      const bottomLastBox = lastBox.offset().top + lastBox.outerHeight();
+      const heightSection =
+        bottomLastBox + padding - Math.abs(section.offset().top);
+      section.css({
+        height: heightSection + 'px',
+      });
     } else {
-      const firstBox = $(container).find('div').first();
+      const firstBox = container.find('div').first();
       const topFirstBox = firstBox.offset().top;
       const padding = topFirstBox - $(this).offset().top;
       const lastBox = $(dividers[0]).prev();
       const bottomLastBox = lastBox.offset().top + lastBox.outerHeight();
       const heightSection =
-        bottomLastBox + padding - Math.abs($(this).offset().top);
+        bottomLastBox + padding - Math.abs(section.offset().top);
       const totalSections = Math.ceil(elements.length / itemsSection);
       const resultHeight = heightSection * totalSections;
 
       const positions = [];
-      const positionSection = $(this).offset().top;
+      const positionSection = section.offset().top;
       positions.push(0);
       $.each(dividers, function () {
-        positions.push($(this).offset().top - positionSection);
+        const divider = $(this);
+        positions.push(divider.offset().top - positionSection);
       });
-      $(this).attr('data-positions', positions.join());
+      section.attr('data-positions', positions.join());
 
       $(container).css('height', resultHeight + 'px');
-      $(this).attr('data-total-sections', totalSections);
-      $(this).css({
+      section.attr('data-total-sections', totalSections);
+      section.css({
         height: heightSection + 'px',
         overflow: 'hidden',
       });
-      $(this).attr('data-total-sections');
+      section.attr('data-total-sections');
     }
 
-    $(this).attr('data-current-section', 0);
+    section.attr('data-current-section', 0);
   });
 };
 
 const recreateSection = function (dataIdSection) {
   const section = $('.sections[data-id=' + dataIdSection + ']');
-  const defaultHeightSection = $(section).attr('data-height-section');
-  const itemsSection = $(section).attr('data-items-section');
+  const defaultHeightSection = section.attr('data-height-section');
+  const itemsSection = section.attr('data-items-section');
 
-  const container = $(section).find('div').first();
-  const elements = $(container).find('.correspondente');
+  const container = section.find('div').first();
+  const elements = container.find('.correspondente');
 
   const dividersRemove = container.find('.section-divider');
   dividersRemove.each(function () {
@@ -80,22 +94,23 @@ const recreateSection = function (dataIdSection) {
   });
 
   $.each(elements, function (index) {
+    const element = $(this);
     let position = index + 1;
     if (position % itemsSection === 0 && position !== elements.length) {
-      $(this).after(
+      element.after(
         '<hr class="section-divider" style="display: flex; width: 100%; margin: 20px 0; border: 0;" />'
       );
     }
   });
 
-  const dividers = $(section).find('.section-divider');
+  const dividers = section.find('.section-divider');
   if (dividers.length == 0) {
-    $(section).css('height', 'auto');
-    $(container).css('height', defaultHeightSection + 'px');
-    $(section).attr('data-total-sections', 0);
-    $(section).attr('data-positions', 0);
+    section.css('height', 'auto');
+    container.css('height', defaultHeightSection + 'px');
+    section.attr('data-total-sections', 0);
+    section.attr('data-positions', 0);
   } else {
-    const firstBox = $(container).find('div').first();
+    const firstBox = container.find('div').first();
     const topFirstBox = firstBox.offset().top;
     const padding = topFirstBox - $(this).offset().top;
     const lastBox = $(dividers[0]).prev();
@@ -105,47 +120,49 @@ const recreateSection = function (dataIdSection) {
     const resultHeight = heightSection * totalSections;
 
     const positions = [];
-    const positionSection = $(section).offset().top;
+    const positionSection = section.offset().top;
     positions.push(0);
     $.each(dividers, function () {
-      positions.push($(this).offset().top - positionSection);
+      const divider = $(this);
+      positions.push(divider.offset().top - positionSection);
     });
-    $(section).attr('data-positions', positions.join());
+    section.attr('data-positions', positions.join());
 
-    $(container).css('height', resultHeight + 'px');
-    $(section).attr('data-total-sections', totalSections);
-    $(section).css({
+    container.css('height', resultHeight + 'px');
+    section.attr('data-total-sections', totalSections);
+    section.css({
       height: heightSection + 'px',
       overflow: 'hidden',
     });
   }
 
-  $(section).attr('data-current-section', 0);
+  section.attr('data-current-section', 0);
 };
 
 const handleChangeSection = function () {
   $('button[data-handle-section]').on('click', function () {
-    const id = $(this).attr('data-handle-section');
+    const button = $(this);
+    const id = button.attr('data-handle-section');
     const section = $('.sections[data-id=' + id + ']');
 
-    const type = $(this).attr('data-type');
-    const totalSections = parseInt($(section).attr('data-total-sections'));
-    const currentSection = parseInt($(section).attr('data-current-section'));
+    const type = button.attr('data-type');
+    const totalSections = parseInt(section.attr('data-total-sections'));
+    const currentSection = parseInt(section.attr('data-current-section'));
 
     const positions = [];
-    let textPositions = $(section).attr('data-positions').split(',');
-    $.each(textPositions, function (index) {
-      positions.push(parseFloat(textPositions[index]));
+    let textPositions = section.attr('data-positions').split(',');
+    $.each(textPositions, function (_, value) {
+      positions.push(parseFloat(value));
     });
 
     let newCurrentSection;
     if (type === 'next' && currentSection < totalSections - 1) {
       newCurrentSection = currentSection + 1;
-      $(section).attr('data-current-section', newCurrentSection);
+      section.attr('data-current-section', newCurrentSection);
       section.scrollTop(positions[newCurrentSection]);
     } else if (type === 'previous' && currentSection > 0) {
       newCurrentSection = currentSection - 1;
-      $(section).attr('data-current-section', newCurrentSection);
+      section.attr('data-current-section', newCurrentSection);
       section.scrollTop(positions[newCurrentSection]);
     }
   });
