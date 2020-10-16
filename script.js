e09b1ba74bc9295af00a0785c335ac9535d8eef1
Guerder Vieira <g7.vieira@gmail.com>
2020-10-16 13:56:28 -0300

    Alterado comportamento do script para possibilitar filtragem pelo atributo data-filter.

--- script.js
+++ script.js
@@ -12,7 +12,12 @@ const createSections = function () {
     const itemsSection = section.attr('data-items-section');
 
     const container = section.find('div').first();
-    const elements = container.children('div');
+
+    const filter = section.attr('data-filter');
+    const elements =
+      filter === undefined
+        ? container.children('div')
+        : container.find('.' + filter);
 
     const dividersRemove = container.find('.section-divider');
     dividersRemove.each(function () {
@@ -32,14 +37,14 @@ const createSections = function () {
     const dividers = section.find('.section-divider');
     if (dividers.length == 0) {
       section.attr('data-total-sections', 0);
-      section.attr('data-total-sections', 0);
       section.attr('data-positions', 0);
 
-      const firstBox = container.find('div').first();
-      const topFirstBox = firstBox.offset().top;
+      const boxesList = container.find('div');
+      const firstBox = boxesList[0];
+      const topFirstBox = firstBox.offsetTop;
       const marginBox = topFirstBox - container.offset().top;
-      const lastBox = container.find('div').last();
-      const bottomLastBox = lastBox.offset().top + lastBox.innerHeight();
+      const lastBox = boxesList[boxesList.length - 1];
+      const bottomLastBox = lastBox.offsetTop + lastBox.innerHeight;
       const heightSection = bottomLastBox + marginBox - container.offset().top;
       const marginContainer = container.offset().top - section.offset().top;
 
@@ -74,65 +79,6 @@ const createSections = function () {
 
     section.attr('data-current-section', 0);
   });
-};
-
-const recreateSection = function (dataIdSection) {
-  const section = $('.sections[data-id=' + dataIdSection + ']');
-  const defaultHeightSection = section.attr('data-height-section');
-  const itemsSection = section.attr('data-items-section');
-
-  const container = section.find('div').first();
-  const elements = container.find('.correspondente');
-
-  const dividersRemove = container.find('.section-divider');
-  dividersRemove.each(function () {
-    $(this).remove();
-  });
-
-  $.each(elements, function (index) {
-    const element = $(this);
-    let position = index + 1;
-    if (position % itemsSection === 0 && position !== elements.length) {
-      element.after(
-        '<hr class="section-divider" style="display: flex; width: 100%; margin: 20px 0; border: 0;" />'
-      );
-    }
-  });
-
-  const dividers = section.find('.section-divider');
-  if (dividers.length == 0) {
-    section.css('height', 'auto');
-    container.css('height', defaultHeightSection + 'px');
-    section.attr('data-total-sections', 0);
-    section.attr('data-positions', 0);
-  } else {
-    const firstBox = container.find('div').first();
-    const topFirstBox = firstBox.offset().top;
-    const padding = topFirstBox - $(this).offset().top;
-    const lastBox = $(dividers[0]).prev();
-    const bottomLastBox = lastBox.offset().top + lastBox.outerHeight();
-    const heightSection = bottomLastBox + padding - $(this).offset().top;
-    const totalSections = Math.ceil(elements.length / itemsSection);
-    const resultHeight = heightSection * totalSections;
-
-    const positions = [];
-    const positionSection = section.offset().top;
-    positions.push(0);
-    $.each(dividers, function () {
-      const divider = $(this);
-      positions.push(divider.offset().top - positionSection);
-    });
-    section.attr('data-positions', positions.join());
-
-    container.css('height', resultHeight + 'px');
-    section.attr('data-total-sections', totalSections);
-    section.css({
-      height: heightSection + 'px',
-      overflow: 'hidden',
-    });
-  }
-
-  section.attr('data-current-section', 0);
 };
 
 const handleChangeSection = function () {
